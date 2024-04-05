06e351c89dce4ce34ad6aeb2e166b6ed2acb9176
Ohihahaha <867651822@qq.com>
2024-04-05 22:29:38 +0800

    修改了方法

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 import json
-from datetime import datetime
 
 class HttpUtils:
     @staticmethod
@@ -8,8 +7,8 @@ class HttpUtils:
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
         }
-        response = requests.get(url, headers=headers, proxies={})  # 禁用代理
-        return response.text
+        response = requests.get(url, headers=headers)
+        return response.json()
 
 class Wallpaper:
     BING_API = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1612409408851&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160"
@@ -17,29 +16,22 @@ class Wallpaper:
 
     @staticmethod
     def main():
-        http_content = HttpUtils.get_http_content(Wallpaper.BING_API)
-        data = json.loads(http_content)
+        data = HttpUtils.get_http_content(Wallpaper.BING_API)
         image_info = data["images"][0]
 
-        # 图片地址
+        # 提取图片信息
         url = Wallpaper.BING_URL + image_info["url"]
-        url = url.split('&')[0]
+        description = image_info["title"]
 
-        # 图片时间
-        end_date = image_info["enddate"]
-
-        # 图片版权
-        copyright = image_info["copyright"]
-
-        # 格式化为 MD 格式
-        text = "{0} | [{1}]({2})\n".format(end_date, copyright, url)
+        # 构造 Markdown 格式的文本
+        text = "## {0} | [{1}](https://cn.bing.com)\n![]({2})\n".format(image_info["enddate"], description, url)
 
         # 读取已存在的内容
         with open("README.md", "r", encoding="utf-8") as file:
             existing_content = file.read()
 
         # 将新内容插入到旧内容之前
-        new_content = "## Bing Wallpaper\n" + text + existing_content
+        new_content = text + existing_content
 
         # 写入 MD 文件，使用 UTF-8 编码
         with open("README.md", "w", encoding="utf-8") as file:
